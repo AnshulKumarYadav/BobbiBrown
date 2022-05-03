1d2eba01f514efc82e2caab9a6da4f17aabff906
Anshul Kumar Yadav <rishabhyadav3602@gmail.com>
2022-05-03 17:30:06 +0530

    footer

---
+++ components/footer.js
@@ -0,0 +1,8 @@
+//<script src="https://kit.fontawesome.com/322b14b2ff.js" crossorigin="anonymous"></script>   ----add this link to your html file
+
+function footer() {
+    return 
+    ``
+}
+
+export {footer};
