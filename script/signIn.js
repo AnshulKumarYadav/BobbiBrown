e16f5e6a6aa536ff891141347020138d4454830c
Harsha-C2508 <harshac2508@gmail.com>
2022-05-04 11:59:11 +0530

    from harsha

---
+++ script/signIn.js
@@ -0,0 +1,5 @@
+import { navbar } from "../components/navbar.js";
+document.getElementById("header").innerHTML = navbar()
+
+import { foot } from "../components/footer.js";
+document.getElementById("footer").innerHTML = foot()
