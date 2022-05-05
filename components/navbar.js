7b025b0e890af8799f8b88d14b59f5ce016692be
Vivek Goswami <vivekgoswamidbng@gmail.com>
2022-05-05 11:05:13 +0530

    final

--- components/navbar.js
+++ components/navbar.js
@@ -23,8 +23,8 @@ function navbar()
         <span class="material-symbols-outlined">search</span>
         <input type="text" placeholder="SEARCH" id="search">
         </div>
-        <h3><a href="../html/new.html">New</a></h3>
-        <h3><a href="./html/new.html">BestSeller</a></h3>
+        <h3><a href="/BobbiBrown/html/new.html">New</a></h3>
+        <h3><a href="/BobbiBrown/html/new.html">BestSeller</a></h3>
         <h3>Face</h3>
         <h3>Makeup</h3>
         <h3>Skincare</h3>
